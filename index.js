22605d512f56e87bd4d6833d7d11a1521f8d14e9
NatSrisuksai <natsrisuksai02@gmail.com>
2024-09-19 20:59:22 +0700

    update : add vercel.json

--- index.js
+++ index.js
@@ -42,13 +42,9 @@ app.use(
 );
 app.use(helmet());
 
-const db = new pg.Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-});
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+})
 
 db.connect();
 
