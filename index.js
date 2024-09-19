1acd977d507d63a1ee48936b9527562078ecf759
NatSrisuksai <natsrisuksai02@gmail.com>
2024-09-19 21:11:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", // eact app's URL
+    origin: true, // eact app's URL
     credentials: true, // Allow cookies to be sent
   })
 );
